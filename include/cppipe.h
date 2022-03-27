d9ee3cbc82392439c58020016c095e8dda5f2b6e
Nikita Eshkeev <neshkeev@yandex.ru>
2022-03-28 00:41:35 +0300

    Initial project placement
    Signed-off-by: Nikita Eshkeev <neshkeev@yandex.ru>

---
+++ include/cppipe.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+void some()
+{
+  std::cout << "HEADER ONLY" << std::endl;
+  return;
+}
---
+++ test/test_suite.cpp
@@ -0,0 +1,10 @@
+#include <cppipe.h>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  some();
+  std::cout << "THIS IS OUTPUT" << std::endl;
+
+  return 0;
+}
